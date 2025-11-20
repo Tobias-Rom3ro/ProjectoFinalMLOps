836931663bf375f31198ea029aefd9512c303ed1
Tobias-Rom3ro <taromero@unimagdanela.edu.co>
2025-11-20 11:27:56 -0500

    refactor: update some rutes to save the cnn model

--- cnn_image/app/core/config.py
+++ cnn_image/app/core/config.py
@@ -1,5 +1,6 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class Settings(BaseSettings):
     service_name: str = "cnn_image"
     log_level: str = "INFO"
@@ -7,15 +8,17 @@ class Settings(BaseSettings):
     host: str = "0.0.0.0"
     port: int = 8002
     
-    mlflow_tracking_uri: str = ""
-    model_path: str = "./models/saved_model.pth"
+    mlflow_tracking_uri: str = "http://mlflow:5000"
+    mlflow_experiment_name: str = "cnn_mnist_classification"
     
-    input_size: int = 28
+    cnn_model_path: str = "models/mnist_cnn_model.keras"
+    image_size: int = 28
     num_classes: int = 10
     
     model_config = SettingsConfigDict(
         env_file=".env",
-        case_sensitive=False
+        case_sensitive=False,
+        protected_namespaces=('settings_',)
     )
 
 
